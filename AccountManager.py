d6ac6a379ae3b2cc9a772804bea7104b662a5dbf
ryanbhang129 <30453652+calculus0129@users.noreply.github.com>
2019-10-18 19:07:04 +0900

    Update AccountManager.py

--- AccountManager.py
+++ AccountManager.py
@@ -4,7 +4,7 @@ class users:
     name = ''
     pw = ''
     IsT = False
-    stake = [10] # a default stake. Has stake for every subjects. 각 과목마다 지분이 존재함
+    stake = [10] # a default stake. Has stake for every subjects. 각 분야(리스트)마다 지분이 존재함
     vrs = [] # verification records. 사용자 인증 기록
 epws = [] # pw encrypted text of name. contains all users' epw.
 

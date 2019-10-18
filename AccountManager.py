1186c787e90e24408b013ec5066656b55bbf5085
ryanbhang129 <30453652+calculus0129@users.noreply.github.com>
2019-10-18 16:09:37 +0900

    Create AccountManager.py

---
+++ AccountManager.py
@@ -0,0 +1,13 @@
+# User accounts and logins
+
+class users:
+    name = ''
+    pw = ''
+    IsT = False
+    stake = [10] # a default stake. Has stake for every subjects. 각 과목마다 지분이 존재함
+    vrs = [] # verification records. 사용자 인증 기록
+epws = [] # pw encrypted text of name. contains all users' epw.
+
+def createacc():
+    #
+    print('something')
